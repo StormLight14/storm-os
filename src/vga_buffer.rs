92ecc7c23a50cca27922f98b4ca8c2e4bbefe22c
storm <stormpower44@protonmail.com>
2023-08-29 21:46:34 -0600

    add custom println macro

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -127,3 +127,20 @@ impl fmt::Write for Writer {
         Ok(())
     }
 }
+
+// ----- macros --------
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    WRITER.lock().write_fmt(args).unwrap();
+}
