27478fd171abe3b4466e2c79df84fcc076806a2d
Storm <stormpower44@protonmail.com>
2023-09-06 18:16:02 -0600

    add serial printing

--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,60 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use vga_buffer::{Color, ColorCode};
+use vga_buffer::Color;
 
+mod serial;
 mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    serial_println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    #[cfg(test)]
-    test_main();
-
     vga_buffer::WRITER.lock().set_foreground(Color::LightRed);
     println!("StormOS");
     vga_buffer::WRITER.lock().set_foreground(Color::LightBlue);
     println!("Hello World!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
+    serial_println!("Running {} tests", tests.len());
     for test in tests {
         test();
     }
+
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
 fn trivial_assertion() {
-    print!("trivial assertion test...");
+    serial_print!("trivial assertion test...");
     assert_eq!(1, 1);
-    println!("[ok]");
+    serial_println!("[ok]");
 }
